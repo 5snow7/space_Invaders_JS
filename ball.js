75df27bdbfc9a032fe80bb0bde6a6cf757abe90e
mark <snowtowind@yahoo.com>
2018-08-02 06:28:03 -0700

    chg color of 3rd var. in ball

--- ball.js
+++ ball.js
@@ -1,7 +1,7 @@
 class ball{
 
 constructor(){this.x=random(width);this.y=random(height);this.xspd=random(5);
-   this.yspd=3;this.cnt=0;
+   this.yspd=3;this.cnt=0;this.col2=floor(random(250));
 }
 
 move(){this.x=this.x+this.xspd;this.y=this.y+this.yspd;}
@@ -9,7 +9,7 @@ checkbd(){
    if(this.x<0||this.x>width){this.xspd=-1*this.xspd;this.cnt++;}
    if(this.y<0||this.y>height){this.yspd=-1*this.yspd;}
  }
-update(){fill(250,0,0);ellipse(this.x,this.y,20,10);
+update(){fill(250,0,this.col2);ellipse(this.x,this.y,20,10);
 }
 isdead(){if(this.cnt>3){return true;}else{return false;}}
 }
